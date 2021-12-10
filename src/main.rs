730967307d06b4b1efc40d7cb344870cea8292c7
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-12-10 02:08:56 +0100

    init commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use rand::Rng;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // genpass 24
+    let length = &args[1];
+
+    let password = generate_pass(length);
+    println!("{}", password);
+}
+
+fn generate_pass(length: &str) -> String {
+    let mut rng = rand::thread_rng();
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password: String = (0..length.parse::<usize>().unwrap())
+        .map(|_| {
+            charset
+                .chars()
+                .nth(rng.gen_range(0, charset.len()))
+                .unwrap()
+        })
+        .collect();
+
+    password
+}
